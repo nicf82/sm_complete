d42ce271c5cac91b919dd9de9a3b0d3fd76ce234
Nic Fellows <nic@nicshouse.co.uk>
2015-06-09 13:58:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Service Manager Bash Completion
+===============================
+
+Prerequsites
+------------
+
+jq, e.g:
+
+    brew install jq
+
+Install
+-------
+
+Execute sm_complete.sh in your ~/.bash_profile e.g:
+
+    . ~/sm_complete.sh
+
---
+++ sm_complete.sh
@@ -0,0 +1,14 @@
+function _sm_complete() {
+    local cur prev opts
+    SM_DIR="$WORKSPACE/service-manager-config" 
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(jq 'keys' $SM_DIR/services.json $SM_DIR/profiles.json | awk -F'"' '{print $2}')
+
+    if [[ ${cur} == * ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+}
+complete -F _sm_complete sm
