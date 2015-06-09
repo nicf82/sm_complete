c017cbd5b48cfbaa62708000cf1afa9630b97950
Nic Fellows <nic@nicshouse.co.uk>
2015-06-09 14:32:52 +0100

    Add switches

--- sm_complete.sh
+++ sm_complete.sh
@@ -4,7 +4,9 @@ function _sm_complete() {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(jq 'keys' $SM_DIR/services.json $SM_DIR/profiles.json | awk -F'"' '{print $2}')
+    services=$(jq 'keys' $SM_DIR/services.json $SM_DIR/profiles.json | awk -F'"' '{print $2}')
+    switches=$(sm --help | tr ' ' '\n' | grep -E '^-.+' | tr -d ',')
+    opts="$services $switches"
 
     if [[ ${cur} == * ]] ; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
